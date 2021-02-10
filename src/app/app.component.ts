08fb0e028562cb93dad91c178eb338feb8eb8fce
Vinicius Fernandes de Paula <vinicim.fernandez1@gmail.com>
2021-02-10 20:57:48 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ photos = [
+   {
+    url:
+    'https://st2.depositphotos.com/1036149/5381/i/600/depositphotos_53811511-stock-photo-duck-with-sunglasses.jpg',
+    description: 'vagalume'
+   },
+   {
+    url:
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Pato.jpg/1200px-Pato.jpg',
+    description: 'patao'
+   }
+ ]
 }
---
+++ src/app/photos/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import { Input } from "@angular/core";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "ap-photo",
+  templateUrl: "photo.component.html"
+})
+export class PhotoComponent{
+
+  @Input() description = '';
+  @Input() url = ''
+
+}
