29d636145415331145aaac13fd9141b3fbe15939
Vinicius Fernandes de Paula <vinicim.fernandez1@gmail.com>
2021-02-17 12:09:37 -0300

    Implementando behaviorSubject e async pipe;

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,19 @@
 import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+import { User } from "../user/user";
+import { UserService } from "../user/user.service";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent{}
+export class HeaderComponent{
+
+    user$: Observable<User>;
+
+
+    constructor(private userService : UserService){
+      this.user$ = this.userService.getUser();
+
+    }
+}
