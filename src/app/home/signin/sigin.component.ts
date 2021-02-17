a12656ee3ee7f4b69ac06ebee2b2c2b9459ba583
Vinicius Fernandes de Paula <vinicim.fernandez1@gmail.com>
2021-02-17 09:25:48 -0300

    Fazendo login da aplicação

--- src/app/home/signin/sigin.component.ts
+++ src/app/home/signin/sigin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "src/app/core/auth.service";
 
 @Component({
   templateUrl: './sigin.component.html'
@@ -8,15 +9,30 @@ export class SiginComponent implements OnInit{
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder){
-
-  }
+  constructor(private formBuilder: FormBuilder,
+              private authService:AuthService)
+              {}
 
   ngOnInit():void{
     this.loginForm = this.formBuilder.group({
       userName: ['',Validators.required],
       password: ['',Validators.required]
     });
+
+  }
+
+  login(){
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+  this.authService
+        .authenticate(userName,password)
+        .subscribe(()=>console.log('AUNTENTICADO'),
+        err=>{
+          console.log(err);
+          this.loginForm.reset();
+        })
+        ;
   }
 
 }
