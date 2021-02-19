90cabf80351f8d76c6e5c80c0116047d6522bf05
Vinicius Fernandes de Paula <vinicim.fernandez1@gmail.com>
2021-02-19 13:52:31 -0300

    implementando o interceptador de token, para envio no header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from "@angular/common";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { RequestInterceptor } from "./auth/request.interceptor";
 import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
   declarations:[HeaderComponent],
   exports: [HeaderComponent],
   imports: [ CommonModule,
-              RouterModule]
+              RouterModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }
+  ]
 })
 export class CoreModule{
 
