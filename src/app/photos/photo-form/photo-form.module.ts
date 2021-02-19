5b95fed6c94c48f27adca2c69d776afecbb80f60
Vinicius Fernandes de Paula <vinicim.fernandez1@gmail.com>
2021-02-19 15:02:38 -0300

    Criando e validando o forms de upload de photos

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { VmessagemModule } from "src/app/shared/components/vmessage/vmessagem.module";
 import { PhotoFormComponent } from "./photo-form.component";
 
 @NgModule({
     declarations: [PhotoFormComponent]
     ,
   imports:[
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    VmessagemModule,
+    FormsModule
                   ]
 })
 export class photoFormModule{}
